1547eabc3309d8d6988497c7dbd9d46b33ab9347
ashrafbadalof <145157534+ashrafbadalof@users.noreply.github.com>
2024-05-05 20:43:49 +0400

    first commit

---
+++ WebAPIProject/WebAPIProject/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIProject.Models
+{
+    public class Users
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string ContactNumber { get; set; }
+
+    }
+}
